04c6f6104a7ff8ba9ce723ccd9b03997ceceb410
VladADragos <vlad6620@yahoo.com>
2019-09-09 11:30:56 +0200

    finished?

--- test.hs
+++ test.hs
@@ -1,63 +1,80 @@
 {- Lab 1
-   Authors:
-   Lab group:
+ Authors: Vlad Alexandru Dragos, Houssam El-Din Boughdadi
+
+ Lab group:
  -}
+ ---------------------------------------------
 
 import MeasureTime
----------------------------------------------
+
 power :: Integer -> Integer -> Integer
 power n k
    | k < 0 = error "power: negative argument"
 power n 0  = 1
 power n k  = n * power n (k-1)
 
+-- A -------------------------
+-- stepsPower n k gives the number of steps that
+-- power n k takes to compute
 
 stepsPower :: Integer -> Integer -> Integer
 stepsPower n k = k+1
 
 
-
-
-
+-- B -------------------------
+-- power1
 power1 :: Integer -> Integer -> Integer
+power1 n k 
+    | k < 0 = error "power: negative argument"
 power1 n k  = product (replicate (fromInteger k) n)
 
 
+-- C -------------------------
+-- power2
 power2 :: Integer -> Integer -> Integer
 power2 n k
+  | k < 0 = error "power: negative argument"
   | even k = power1 (n * n) (div k 2)
   | odd k = n * (power1 n (k - 1))
 
 
 
-  
+-- D -------------------------
+{- 
 
--- test 0,1, odd anv even
--- number should be uint
+<Describe your test cases here>
+The test function compares the results from the different power functon using bases from -100 to 100 and exponents from 0 to 100
 
-boolToStr :: Bool -> String
-boolToStr True = "True"
-boolToStr False = "False"
+ -}
 
--- test1 = putStrLn "Hello" putStrLn "World"
+-- testFunc range parameters
+minExponent :: Integer
+minExponent = 0
+
+maxExponent :: Integer
+maxExponent = 100
 
 
+minBase :: Integer
+minBase = (-100)
 
+maxBase :: Integer
+maxBase = 100
+ 
+ 
+-- comparePower1
 comparePower1 :: Integer -> Integer -> Bool
 comparePower1 n k = (power n k) == (power1 n k)
+ 
 
-
+-- comparePower2
 comparePower2 :: Integer -> Integer -> Bool
 comparePower2 n k = (power n k) == (power2 n k)
+ 
 
+comparePowers :: Integer -> Integer -> Bool
+comparePowers n k = (comparePower1 n k) == (comparePower2 n k)
 
-comp :: Integer -> Integer -> Bool
-comp n k = (comparePower1 n k) == (comparePower2 n k)
-
-
-minimumm = 0
-maximumm = 100
-
-test :: Bool
-test = not (elem False [comp n k | n <- [-100..100], k <- [0..100]]) 
-
+-- Test functions: 
+testFunc :: Bool
+testFunc = not (elem False [comparePowers n k | n <- [minBase..maxBase], k <- [minExponent..maxExponent]]) 
